6869b8b0ebe553881fa67f43bc9d6837923528bf
XanderWang <420640763@qq.com>
2021-02-26 12:50:49 +0800

    test

--- blog_config/gitee.py
+++ blog_config/gitee.py
@@ -5,7 +5,8 @@ GITEE_NAME = os.environ["GITEE_NAME"]
 GITEE_EMAIL = os.environ["GITEE_EMAIL"]
 GITEE_PATH = os.environ["GITEE_PATH"]
 
-print("GITEE_NAME:", GITEE_NAME, ",GITEE_EMAIL:", GITEE_EMAIL)
+print("GITEE_NAME:", GITEE_NAME)
+print("GITEE_EMAIL:", GITEE_EMAIL)
 print("GITEE_PATH:", GITEE_PATH)
 
 
@@ -16,18 +17,17 @@ def doGit(git_cmd):
 
 # 设置邮箱等配置
 cmd_list = [
+    "git remote rm origin",
     "git config --local user.name {0}".format("420640763@qq.com"),
     "git config --local user.email {0}".format("XanderWang"),
-    "git remote rm origin",
     "git remote add origin {0}".format(GITEE_PATH),
     "git config --list",
     "git add ./",
     "git commit -m \"auto build task\"",
 ]
 
-try :
+try:
     for cmd in cmd_list:
         doGit(cmd)
 except Exception as e:
     print(e)
-
