4c41bb6a28c302c46ea0d19426dc560bb8d70167
XanderWang <420640763@qq.com>
2021-09-25 23:56:17 +0800

    change config

--- blog_config/build_tencent_webify.sh
+++ blog_config/build_tencent_webify.sh
@@ -1,6 +1,11 @@
 
 echo '============= start build.sh ==================='
 ls -la 
+echo '============= change config ==================='
+rm ./_config.yml
+cp ./blog_config/_config_tencent_webify.yml ./_config.yml
+rm ./_config.volantis.yml
+cp ./blog_config/_config.volantis_tencent_webify.yml ./_config.volantis.yml
 echo '------------------------------- start npm install '
 # 安装依赖的环境
 # npm install
